df065eab0b5ea2633b5ecdff055dd53a40f2b010
Sarah Smith <hmotammedi@gmail.com>
2022-04-04 13:24:59 +0430

    DIG-400 add api response

--- src/api.py
+++ src/api.py
@@ -1,17 +1,26 @@
-from flask import Flask
-
-
+from flask import Flask, Response
 from flask_cors import CORS
+import os
+import pandas as pd
+
 
 app = Flask(__name__)
 CORS(app)
 
+training_data = pd.read_csv(os.path.join('data', 'auto-mpg-training-data.csv'))
+print(training_data)
 
-@app.route("/", methodes=["GET"])
+
+@app.route("/", methods=["GET"])
 def hello_world():
     return {"hello", "World "}
 
 
-@app.route("/hello_world", methodes=["GET"])
+@app.route("/hello_world", methods=["GET"])
 def hello_word():
     return "<p>Hello World!, </>"
+
+
+@app.route("/training_data", methods=["GET"])
+def get_training_data():
+    return Response(training_data.to_json(), mimetype='application/json')
