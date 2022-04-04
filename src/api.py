17ffb273e904cf5b09c8ce090fb59ca650b0cfe5
Sarah Smith <hmotammedi@gmail.com>
2022-04-04 12:22:46 +0430

    DIG-396 add requirements

--- src/api.py
+++ src/api.py
@@ -6,6 +6,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/",methodes=["GET"])
+
+@app.route("/", methodes=["GET"])
 def hello_world():
     return {"hello", "World "}
+
+
+@app.route("/hello_world", methodes=["GET"])
+def hello_word():
+    return "<p>Hello World!, </>"
